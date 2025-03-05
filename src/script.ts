fc86d94a996e269c1b8f1ef4bb0e5d2ce47af559
Gabriel Sousa <gabriel1908bs@gmail.com>
2025-03-05 10:16:12 -0300

    More update Typescipt

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,41 @@
 const canvas = document.getElementById('jogo2D') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
+document.addEventListener('keydown', (e) => {
+    if(e.code == 'Space') {
+        console.log("Press the Space Bar")
+        personagem.speedy = 15
+        personagem.jump = true
+    }
+})
+
+const personagem = {
+    x: 100,
+    y:canvas.height - 50,
+    height: 50,
+    width: 50,
+    jump: false,
+    speedy: 0,
+    speedx: 10
+}
+
 function desenharPersonagem(){
     ctx.fillStyle = 'white'
-    ctx.fillRect(100,25,50,50);
+    ctx.fillRect(personagem.x,personagem.y,personagem.height,personagem.width);
+}
+
+function AtualizarPersonagem(){
+    if(personagem.jump == true){
+        personagem.y += personagem.speedy;
+    }
 }
 
 function loop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
     desenharPersonagem();
+    AtualizarPersonagem()
+    requestAnimationFrame(loop)
 }
 
 loop()
