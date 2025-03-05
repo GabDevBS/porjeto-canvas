cef2f3a60521ad11620557f9399d55dbf7b796fd
Gabriel Sousa <gabriel1908bs@gmail.com>
2025-03-05 09:45:16 -0300

    Fix bugs, update TypeScript

--- out/script.js
+++ out/script.js
@@ -1,6 +1,12 @@
 "use strict";
 const canvas = document.getElementById('jogo2D');
 const ctx = canvas.getContext('2d');
-function loop() {
+function desenharPersonagem() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(100, 25, 50, 50);
 }
+function loop() {
+    desenharPersonagem();
+}
+loop();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,13 @@
 const canvas = document.getElementById('jogo2D') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d')!;
 
-function loop(){
-    
+function desenharPersonagem(){
+    ctx.fillStyle = 'white'
+    ctx.fillRect(100,25,50,50);
 }
 
+function loop(){
+    desenharPersonagem();
+}
+
+loop()
